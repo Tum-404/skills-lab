ada00c77e6cfdb1a73d17e130bee428f497e5c30
tum <tamvalove@gmail.com>
2025-11-17 08:28:25 +0700

    feat: add Cities module for fetch data from database

--- non-ai-coding-assistant-nest-postgres/src/app.module.ts
+++ non-ai-coding-assistant-nest-postgres/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path/win32';
+import { CitiesModule } from './cities/cities.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT') || 5432,
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [join(__dirname, '/**/*.entity{.ts,.js}')],
+        synchronize: true,
+      }),
+    }),
+    CitiesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
